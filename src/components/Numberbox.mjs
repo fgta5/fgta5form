4863cabd20f77e5daa6bc5e36d4d66f4d396b81c
agungdhewe <agung_dhewe@yahoo.com>
2025-05-26 11:38:28 +0700

    dw 20250526 1138 start creating numberbox

--- src/components/Numberbox.mjs
+++ src/components/Numberbox.mjs
@@ -9,13 +9,143 @@ export default class Numberbox extends Input {
 		Numberbox_construct(this, id)
 		Numberbox_readValidators(this)
 	}
+
+	get Disabled() { return this.Element.disabled }
+	set Disabled(v) { 
+		this.Element.disabled = v 
+		Numberbox_setDisabled(this, v)
+	}
+
+	SetEditingMode(ineditmode) {
+		Numberbox_SetEditingMode(this, ineditmode)
+	}
+
 }
 
 function Numberbox_construct(self, id) {
+	const container = document.createElement('div')
+	const wrapinput = document.createElement('div')
+	const input = self.Element
+	const display = document.createElement('input')
+	const lstvalue = document.createElement('input')
+	const label = document.querySelector(`label[for="${self.Id}"]`)
+	
+
+	container.classList.add('fgta5-entry-container')
+	wrapinput.classList.add('fgta5-entry-input-wrapper')
+	input.classList.add('fgta5-entry-input')
+	display.classList.add('fgta5-entry-display')
+	lstvalue.classList.add('fgta5-entry-lastvalue')
+
+	input.parentNode.insertBefore(container, input)
+	container.appendChild(wrapinput)
+	wrapinput.appendChild(display)
+	wrapinput.appendChild(input)
+	container.appendChild(lstvalue)
+
+	display.setAttribute('type', 'number')
+	input.setAttribute('type', 'hidden')
+	lstvalue.setAttribute('type', 'hidden') 
+
+
+	// set input value
+	lstvalue.value = input.value
+
+	// set input description
+	var description = self.Element.getAttribute('description')
+	if (description !== null && description.trim() !== '') {
+		description = description.trim()
+		const decrdiv = document.createElement('div')
+		decrdiv.classList.add('fgta5-entry-description')
+		decrdiv.innerHTML = description
+		container.appendChild(decrdiv)
+	}
+
+
+	display.addEventListener('focus', function(e) {
+		Numberbox_displayFocus(self, e)
+	})
+
+	display.addEventListener('blur', function(e) {
+		Numberbox_displayBlur(self, e)
+	})
+
+	display.addEventListener('change', function(e) {
+		Numberbox_displayChanged(self, e)
+	})
+
+	display.addEventListener("input", function(event) {
+		if (display.value !== lstvalue.value) {
+			display.setAttribute('changed', 'true')
+		} else {
+			display.removeAttribute('changed')
+		}
+	})
+
+
+	self.Nodes = {
+		Container: container,
+		InputWrapper: wrapinput,
+		Input: input,
+		Display: display,
+		LastValue: lstvalue,
+		Label: label
+	}	
 
 }
+
+function Numberbox_setDisabled(self, v) {
+	if (v) {
+		self.Nodes.Display.disabled = true
+	} else {
+		self.Nodes.Display.disabled = false
+	}
+}
+
+function Numberbox_SetEditingMode(self, ineditmode) {
+	var attrval = ineditmode ? 'true' : 'false'
+
+	self.Nodes.Display.setAttribute('editmode', attrval)
+	self.Nodes.Input.setAttribute('editmode', attrval)
+	self.Nodes.InputWrapper.setAttribute('editmode', attrval)
+
+	if (ineditmode) {
+		self.Nodes.Input.removeAttribute('readonly')
+		self.Nodes.Display.removeAttribute('readonly')
+	} else {
+		self.Nodes.Input.setAttribute('readonly', 'true')
+		self.Nodes.Display.setAttribute('readonly', 'true')
+		self.SetError(null)
+	}
+}
+
+function Numberbox_displayFocus(self, e) {
+	var display = self.Nodes.Display
+	var input = self.Nodes.Input
+	console.log('display focus')
+}
+
+function Numberbox_displayBlur(self, e) {
+	var display = self.Nodes.Display
+	var input = self.Nodes.Input
+	console.log('display blur')
+}
+
+function Numberbox_displayChanged(self, e) {
+	var display = self.Nodes.Display
+	var input = self.Nodes.Input
+	
+	// set nilai input menjadi nilai display
+	input.value = display.value
+
+	
+	
+
+}
+
 
 
 function Numberbox_readValidators(self) {
 
 }
+
