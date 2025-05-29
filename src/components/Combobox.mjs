f8e1f6697d09f701ac56a2b9f8f6938512ef3436
agungdhewe <agung_dhewe@yahoo.com>
2025-05-30 06:19:57 +0700

    dw 20250530 0619

--- src/components/Combobox.mjs
+++ src/components/Combobox.mjs
@@ -5,7 +5,6 @@ export default class Combobox extends Input {
 
 	constructor(id) {
 		super(id)
-
 		Combobox_construct(this, id)
 	}
 
@@ -13,5 +12,37 @@ export default class Combobox extends Input {
 
 
 function Combobox_construct(self, id) {
+	const container = self.Nodes.Container
+	const lastvalue = self.Nodes.LastValue
+	const input = self.Nodes.Input
+	const wrapinput = document.createElement('div')
+	const display = document.createElement('input')
+	const button = document.createElement('button')
+	const label = document.querySelector(`label[for="${id}"]`)
+	const datalist = input.parentNode.querySelector(`datalist[for="${id}"]`)
+
+	input.classList.add('fgta5-entry-input')
+	input.parentNode.insertBefore(container, input)
+	input.setAttribute('type', 'hidden')
+
+	wrapinput.classList.add('fgta5-entry-input-wrapper')
+	display.classList.add('fgta5-entry-display')
+	button.classList.add('fgta5-entry-button-combobox')	
+
+	wrapinput.appendChild(input)
+	wrapinput.appendChild(display)
+	wrapinput.appendChild(button)
+	container.appendChild(wrapinput)
+	container.appendChild(lastvalue)
+
+
+	display.id = self.Id + '-display'
+	display.required = input.required
+	display.setAttribute('style', input.getAttribute('style') || '')
+	display.setAttribute('type', 'text')
+	display.setAttribute('fgta5-component', 'Combobox')
+
+
+	label.setAttribute('for', display.id)
 
 }
