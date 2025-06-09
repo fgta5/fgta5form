bdd8aff67fd66440df4c7f7a877b4da835bfd3a2
agungdhewe <agung_dhewe@yahoo.com>
2025-06-09 15:24:17 +0700

    build v1.0.0

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,14 @@
 import { terser } from "rollup-plugin-terser";
 import postcss from "rollup-plugin-postcss";
 
+const version = '1.0.0'
+
 export default {
   input: "build.mjs", // File utama yang menjadi entry point
   output: {
-    file: "dist/fgta5.min.js", // Lokasi output file hasil bundle
+    file: `dist/fgta5-v${version}-min.js`, // Lokasi output file hasil bundle
     format: "esm", // Format modul ECMAScript
+	banner: `/*! Versi ${version} */`
   },
   plugins: [
     terser({
@@ -14,9 +17,10 @@ export default {
 		}	
 	}),
 	postcss({
-		extract: "fgta5.min.css", // Nama file CSS yang diekstrak
+		extract: `fgta5-v${version}-min.css`, // Nama file CSS yang diekstrak
 		minimize: true, // Minifikasi CSS
 		plugins: [require("cssnano")], // Gunakan cssnano untuk minifikasi
+		banner: `/*! Versi ${version} */`
 	  }),
   ],
 };
